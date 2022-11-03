cc0a5fb87b706d389202db1903ab44f529c88a09
Hyper-terminal <abhisharma001001001@gmail.com>
2022-11-03 12:11:40 +0530

    getbyclassname and tagname

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 
+// by class name
+// let liList = document.getElementsByClassName('list-group-item');
 
-let liList = document.getElementsByClassName('list-group-item');
+// for (let index = 0; index < liList.length; index++) {
+//     liList[index].style.backgroundColor = "lightgrey";
+//     liList[index].style.fontWeight = "bold";
+// }
+
+
+// by tag name
+let liList = document.getElementsByTagName('li');
 
 for (let index = 0; index < liList.length; index++) {
     liList[index].style.backgroundColor = "lightgrey";
     liList[index].style.fontWeight = "bold";
+    liList[index].style.color = 'green';
 }
