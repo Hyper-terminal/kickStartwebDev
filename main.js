3baf6399ac5c9158da77e0e46e4e1c2d39fc7383
Hyper-terminal <abhisharma001001001@gmail.com>
2022-11-03 13:14:17 +0530

    delete functionality

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-var itemList=document.querySelector('#items')
-//console.log(itemList.parentNode);
-//itemList.parentNode.style.backgroundColor='#f4f4f4';
-//console.log(itemList.children[1])
-//itemList.children[1].style.backgroundColor='yellow';
-//console.log(itemList.childNodes)
-//console.log(itemList.firstChild)
-//itemList.firstChild.textContent='HELLO1'
-//console.log(itemList.lasttChild)
-//itemList.lastChild.textContent='HELLO2'
-//console.log(itemList.nextSibling)
-//console.log(itemList.previousSibling);
-//console.log(itemList.previousElementSibling)
-//itemList.previousElementSibling.style.backgroundColor='red';
-var newDiv=document.createElement('div');
-//newDiv.className='Hello';
-//newDiv.id='hello1';
-newDiv.setAttribute('title','hellodiv')
+let ul = document.querySelector("#items");
+let deleteButton = document.querySelector(".delButton");
+
+// delete function
+
+deleteButton.addEventListener("click", () => {
+  if (ul.children.length === 0) {
+    alert("Nothing to delete");
+  } else {
+    ul.lastElementChild.remove();
+  }
+});
