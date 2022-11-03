0988696775f0ad6a20cf6284371cb1729957426c
Hyper-terminal <abhisharma001001001@gmail.com>
2022-11-03 12:40:10 +0530

    Create and modify nodes

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-let li = document.querySelectorAll('.list-group-item');
-
-
-li[1].style.background = 'green';
-// li[2].style.display = 'none';
-
-
-for(let i=1; i<li.length; i+=2){
-    li[i].style.background = 'gray';
-}
-
-
-// queryselectorall will give us the array of objects 
-/// queryselector will select only one ele
+var itemList=document.querySelector('#items')
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor='#f4f4f4';
+//console.log(itemList.children[1])
+//itemList.children[1].style.backgroundColor='yellow';
+//console.log(itemList.childNodes)
+//console.log(itemList.firstChild)
+//itemList.firstChild.textContent='HELLO1'
+//console.log(itemList.lasttChild)
+//itemList.lastChild.textContent='HELLO2'
+//console.log(itemList.nextSibling)
+//console.log(itemList.previousSibling);
+//console.log(itemList.previousElementSibling)
+//itemList.previousElementSibling.style.backgroundColor='red';
+var newDiv=document.createElement('div');
+//newDiv.className='Hello';
+//newDiv.id='hello1';
+newDiv.setAttribute('title','hellodiv')
